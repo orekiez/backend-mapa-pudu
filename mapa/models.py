f9e6c97bc02ebd20543fea57a9bfb6a6487a9534
orekiez <mreinaldo818@gmail.com>
2025-12-16 01:28:45 -0300

    Agregando fecha de creacion al modelo

--- mapa/models.py
+++ mapa/models.py
@@ -9,40 +9,36 @@ class PuntoReciclaje(models.Model):
     estado_llenado = models.IntegerField(default=0) 
     tipo_residuo = models.CharField(max_length=50, default="Vidrio")
     
-    # NUEVO CAMPO: Memoria de cuándo se vació por última vez
+    # Campo para el cálculo predictivo
     fecha_ultimo_vaciado = models.DateTimeField(default=timezone.now)
+
+    # --- NUEVO CAMPO: FECHA DE CREACIÓN ---
+    # auto_now_add=True hace que se guarde la hora exacta solo al crear
+    fecha_creacion = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return self.nombre
 
-    # --- LÓGICA DE PREDICCIÓN (IA MATEMÁTICA) ---
+    # --- LÓGICA DE PREDICCIÓN ---
     @property
     def estimacion_dias(self):
-        # 1. Casos obvios donde no calculamos
         if self.estado_llenado == 0:
             return "Recién vaciado"
         if self.estado_llenado >= 90:
             return "¡Ya está lleno!"
 
-        # 2. Calcular tiempo real transcurrido
         ahora = timezone.now()
         tiempo_pasado = ahora - self.fecha_ultimo_vaciado
-        dias_pasados = tiempo_pasado.total_seconds() / 86400 # Segundos a Días
+        dias_pasados = tiempo_pasado.total_seconds() / 86400 
         
-        # Evitar errores si se acaba de crear hace 1 segundo
         if dias_pasados < 0.001: 
             return "Calculando..."
 
-        # 3. Calcular Velocidad (% que se llena por día)
         velocidad_diaria = self.estado_llenado / dias_pasados
-
-        # 4. Proyectar al futuro (¿Cuándo llega al 90%?)
         porcentaje_restante = 90 - self.estado_llenado
         
         if velocidad_diaria > 0:
             dias_restantes = porcentaje_restante / velocidad_diaria
-            
-            # Formato amigable de respuesta
             if dias_restantes < 1:
                 horas = int(dias_restantes * 24)
                 return f"{horas} horas aprox."
